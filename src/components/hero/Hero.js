9da88daf47b562875fe652e81983a0a8497e776a
Tilwa <shahrukhaltaf123@gmail.com>
2024-08-17 19:31:56 +0500

    init

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Hero.css";
+import Navbar from "../navbar/Navbar";
+const Hero = () => {
+  return (
+    <div>
+      <Navbar />
+      Hero
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/page/home/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css";
+import Navbar from "../../components/navbar/Navbar";
+import Hero from "../../components/hero/Hero";
+import About from "../../components/about/About";
+import Projects from "../../components/allProjects/Projects";
+import Services from "../../components/allServices/Services";
+import Experience from "../../components/experience/Experience";
+import Contact from "../../components/contact/Contact";
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Projects />
+      <Services />
+      <Experience />
+      <Contact />
+    </div>
+  );
+};
+
+export default Home;
